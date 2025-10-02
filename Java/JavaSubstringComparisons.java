424852cce0acac9ca103f42cee26e8085a361bae
Kisaja Beddawala <kisajab72@gmail.com>
2025-10-02 23:22:54 +0530

    complete java substring comparisons challenge

---
+++ Java/JavaSubstringComparisons.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class JavaSubstringComparisons {
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        String sub1 = s.substring(0,k);
+        largest = sub1;
+        smallest = sub1;
+        
+        for(int i = 1; i < s.length() + 1 - k; i++){
+            String substr = s.substring(i, i + k);
+            if (substr.compareTo(smallest) < 0) {
+                smallest = substr;
+            }
+            if (substr.compareTo(largest) > 0) {
+                largest = substr;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
